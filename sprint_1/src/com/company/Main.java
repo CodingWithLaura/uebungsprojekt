516485046d40255f6110bf76635aa12584d037cb
Laura Ganss <laura.ganss@lise.de>
2021-11-30 13:33:37 +0100

    Implement script which calculates the mean value of 6 numbers from user input

--- sprint_1/src/com/company/Main.java
+++ sprint_1/src/com/company/Main.java
@@ -5,10 +5,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Enter two numbers which will be added up: ");
-        Scanner input = new Scanner(System.in);
-        int number_one = input.nextInt();
-        int number_two = input.nextInt();
-        System.out.print(number_one + number_two);
+        Scanner in = new Scanner(System.in);
+        int[] vars = new int[6];
+        System.out.println("Enter " + vars.length + " vars: ");
+        float total = 0;
+        for (int i = 0; i < vars.length; i++) {
+            vars[i] = in.nextInt();
+            total = total + vars[i];
+        }
+        float average = total/vars.length;
+        System.out.println("The mean value of your input is: " + average);
     }
 }
