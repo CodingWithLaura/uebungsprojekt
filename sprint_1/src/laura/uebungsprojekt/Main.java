bea4504630ac3bc464e65d52e70128434757ab50
CodingWithLaura <laura.ganss@web.de>
2021-12-08 09:45:36 +0100

    Implement function which calcs min and max value of input list

--- sprint_1/src/laura/uebungsprojekt/Main.java
+++ sprint_1/src/laura/uebungsprojekt/Main.java
@@ -1,6 +1,5 @@
 package laura.uebungsprojekt;
 
-import laura.uebungsprojekt.aufgaben.UPLK78_Messprogramm;
 import laura.uebungsprojekt.aufgaben.UPLK80_Wetterstation;
 
 public class Main {
@@ -8,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         int[] temps = {12, 14, 9, 12, 15, 16, 15, 15, 11, 8, 13, 13, 15, 12};
         UPLK80_Wetterstation.calc_mean_temp(temps);
+        UPLK80_Wetterstation.calc_min_and_max_temp(temps);
     }
 }
--- sprint_1/src/laura/uebungsprojekt/aufgaben/UPLK80_Wetterstation.java
+++ sprint_1/src/laura/uebungsprojekt/aufgaben/UPLK80_Wetterstation.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class UPLK80_Wetterstation {
 
-    public static void calc_mean_temp(int[] temps){
+    public static void calc_mean_temp(int[] temps) {
         float total = 0;
         float mean = 0;
         for (int i = 0; i < temps.length; i++) {
@@ -13,4 +13,19 @@ public class UPLK80_Wetterstation {
         }
         System.out.println("The mean value of your input is: " + mean);
     }
+
+    public static void calc_min_and_max_temp(int[] temps) {
+        int min = temps[0];
+        int max = 0;
+        for (int temp : temps) {
+            if (min > temp) {
+                min = temp;
+            }
+            if (max < temp) {
+                max = temp;
+            }
+        }
+        System.out.println("The max value of your input is: " +  min + " " + "The min value of your input is: " + max);
+    }
+
 }
