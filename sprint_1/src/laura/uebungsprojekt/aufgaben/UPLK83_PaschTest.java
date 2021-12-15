994620d189ad02afc8cfb7d81d17c674e31d4e28
CodingWithLaura <laura.ganss@web.de>
2021-12-15 13:28:48 +0100

    Write unittests and extract print out method

---
+++ sprint_1/src/laura/uebungsprojekt/aufgaben/UPLK83_PaschTest.java
@@ -0,0 +1,34 @@
+package laura.uebungsprojekt.aufgaben;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UPLK83_PaschTest {
+
+    @Test
+    void diceRoll_method_should_return_true_if_rolled_a_triple() {
+        // Arrange
+        int dice_one = 4;
+        int dice_two = 4;
+        int dice_three = 4;
+
+        // Act
+        boolean result = UPLK83_Pasch.diceRoll(dice_one, dice_two, dice_three);
+
+        // Assert
+        assertTrue(result);
+    }
+    @Test
+    void diceRoll_method_should_return_false_if_not_rolled_a_triple() {
+        // Arrange
+        int dice_one = 2;
+        int dice_two = 4;
+        int dice_three = 4;
+
+        // Act
+        boolean result = UPLK83_Pasch.diceRoll(dice_one, dice_two, dice_three);
+
+        // Assert
+        assertFalse(result);
+    }
+}
