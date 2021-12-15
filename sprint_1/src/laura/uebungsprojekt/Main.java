77713fcc120043ac9b7814066ee6708eb351e0e6
CodingWithLaura <laura.ganss@web.de>
2021-12-15 13:28:48 +0100

    Write unittests and extract print out method

--- sprint_1/src/laura/uebungsprojekt/Main.java
+++ sprint_1/src/laura/uebungsprojekt/Main.java
@@ -3,6 +3,8 @@ package laura.uebungsprojekt;
 import laura.uebungsprojekt.aufgaben.UPLK82_Schaltjahrprüfung;
 import laura.uebungsprojekt.aufgaben.UPLK83_Pasch;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -13,6 +15,12 @@ public class Main {
         //UPLK80_Wetterstation.calc_temp_diff_of_two_days_oo(temps);
 
         //UPLK82_Schaltjahrprüfung.checkIfYearIsLeapYear();
-        UPLK83_Pasch.diceRoll();
+        Random dice = new Random();
+        int dice_one = dice.nextInt(7);
+        int dice_two = dice.nextInt(7);
+        int dice_three = dice.nextInt(7);
+        boolean result = UPLK83_Pasch.diceRoll(dice_one, dice_two, dice_three);
+        UPLK83_Pasch.printResult(result);
+
     }
 }
--- sprint_1/src/laura/uebungsprojekt/aufgaben/UPLK83_Pasch.java
+++ sprint_1/src/laura/uebungsprojekt/aufgaben/UPLK83_Pasch.java
@@ -3,14 +3,18 @@ package laura.uebungsprojekt.aufgaben;
 import java.util.Random;
 
 public class UPLK83_Pasch {
-    public static void diceRoll() {
-        Random dice = new Random();
-        int dice_one = dice.nextInt(7);
-        int dice_two = dice.nextInt(7);
-        int dice_three = dice.nextInt(7);
+    public static boolean diceRoll(int dice_one, int dice_two, int dice_three) {
         System.out.println("dice 1: " + dice_one + " dice 2: " + dice_two + " dice 3: " + dice_three);
         if (dice_one == dice_two && dice_two == dice_three) {
-            System.out.println("Du hast einen Pasch gewürfelt!");
-        } else System.out.println("Du hast keinen Pasch gewürfelt!");
+            return true;
+        } else return false;
+    }
+
+    public static void printResult(boolean result) {
+        if (result) {
+            System.out.println("Du hast einen Pasch gewürfelt");
+        } else {
+            System.out.println("Du hast keinen Pasch gewürfelt");
+        }
     }
 }
