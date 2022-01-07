b9686ea60adaf28414b0edc000d658e819f669df
CodingWithLaura <laura.ganss@web.de>
2022-01-07 14:19:54 +0100

    Add class Elternteil which inherits from class Person and make reden polymorph

--- sprint_1/src/laura/uebungsprojekt/aufgaben/objektorientierung/Main.java
+++ sprint_1/src/laura/uebungsprojekt/aufgaben/objektorientierung/Main.java
@@ -3,7 +3,18 @@ package laura.uebungsprojekt.aufgaben.objektorientierung;
 public class Main {
 
     public static void main(String[] args) {
-        Schüler schüler = new Schüler("Michael", 13, "", "","");
+        Schüler schüler1 = new Schüler("Michael", 13, "", "","Mathe");
+        Schüler schüler2 = new Schüler("Daniel", 13, "", "","Bio");
+        Schüler schüler3 = new Schüler("Lisa", 13, "", "","Musik");
 
+        Elternteil papa = new Elternteil("Heinz", 54, "", "");
+        Elternteil mama = new Elternteil("Henriette", 54, "", "");
+
+        Person[] personen = {schüler1, schüler2, schüler3, papa, mama};
+
+        for (Person person : personen)
+        {
+            person.reden();
+        }
     }
 }
