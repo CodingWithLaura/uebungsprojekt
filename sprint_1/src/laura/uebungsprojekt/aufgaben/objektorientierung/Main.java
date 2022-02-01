7379b96f13b271531123fd90fc2b23ac7f5c9ff5
CodingWithLaura <laura.ganss@web.de>
2022-02-01 14:46:11 +0100

    Create strategy pattern example

--- sprint_1/src/laura/uebungsprojekt/aufgaben/objektorientierung/Main.java
+++ sprint_1/src/laura/uebungsprojekt/aufgaben/objektorientierung/Main.java
@@ -1,9 +1,24 @@
 package laura.uebungsprojekt.aufgaben.objektorientierung;
 
+import laura.uebungsprojekt.aufgaben.objektorientierung.designpatterns.Context;
+import laura.uebungsprojekt.aufgaben.objektorientierung.designpatterns.OperationAdd;
+import laura.uebungsprojekt.aufgaben.objektorientierung.designpatterns.OperationMultiply;
+import laura.uebungsprojekt.aufgaben.objektorientierung.designpatterns.OperationSubstract;
+
 public class Main {
 
     public static void main(String[] args) {
-        Schüler schüler = new Schüler("Michael", 13, "", "","");
+        /*Schüler schüler = new Schüler("Michael", 13, "", "","");*/
 
+        Context context = new Context(new OperationAdd());
+        System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+
+        context = new Context(new OperationSubstract());
+        System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
+
+        context = new Context(new OperationMultiply());
+        System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
     }
+
 }
+
---
+++ sprint_1/src/laura/uebungsprojekt/aufgaben/objektorientierung/designpatterns/Strategy.java
@@ -0,0 +1,5 @@
+package laura.uebungsprojekt.aufgaben.objektorientierung.designpatterns;
+
+public interface Strategy {
+    public int doOperation(int num1, int num2);
+}
